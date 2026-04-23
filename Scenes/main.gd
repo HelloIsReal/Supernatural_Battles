6e610d80beb237d9521af492a614e1e2590c3699
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-04-24 00:48:46 +1200

    Made platform not bug out when using it

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,13 +1,17 @@
 extends Node3D
 
-@export var arrivalTime = 45
+@export var arrivalTime = 60
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$capsule/countdownTimer/Timer.wait_time = arrivalTime
-	$capsule/countdownTimer/Timer.start()
+	$timeBeforePlatformMove.start()
+
+func _physics_process(delta):
+	$capsule/countdownTimer/Label3D.text = "T-Minus: \n %.1f" %  $Timer.time_left
+
+
+func _on_time_before_platform_move_timeout():
+	$Timer.wait_time = arrivalTime
+	$Timer.start()
 	var tween = get_tree().create_tween()
 	tween.tween_property($capsule, "position", Vector3(0,0,0), arrivalTime)
-
-func _process(delta: float) -> void:
-	$capsule/countdownTimer/Label3D.text = "T-Minus: \n %.1f" %  $capsule/countdownTimer/Timer.time_left
