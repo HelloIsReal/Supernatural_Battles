4236d1581a0d865384fdaea049dbd4cdc3b97846
SreekTi <sreekti@whs.school.nz>
2026-05-08 09:36:02 +1200

    Made enemy projectiles now work

--- enemy_projectile.gd
+++ enemy_projectile.gd
@@ -1,14 +1,15 @@
 extends Node3D
 
-@onready var player = get_tree().get_first_node_in_group("testTargetGroup")
+@onready var player = get_tree().get_first_node_in_group("player")
 var homingBullet=true
-var projectileSpeed=5
-
+var projectileSpeed=15
+var bodySize = 0.1 # offsets the orbs to hit the player body and not feet.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if(homingBullet):
-		position += position.direction_to(player.position) * projectileSpeed * delta
+		position -= global_position.direction_to(Vector3(player.global_position.x, player.global_position.y, player.global_position.z)).normalized() * projectileSpeed * delta
+		#position = position.lerp(player.position, 1 * delta)
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	print(area)
