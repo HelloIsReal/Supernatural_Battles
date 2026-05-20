76b7d300e1af8b53e5878f66f0b5482459269f2e
SreekTi <sreekti@whs.school.nz>
2026-05-21 10:38:16 +1200

    Slightly more progress on level1
    Grass focused

--- Scenes/level_1.gd
+++ Scenes/level_1.gd
@@ -1,21 +1,22 @@
-extends MultiMeshInstance3D
+extends Node3D
+#extends MultiMeshInstance3D
 
-func _ready():
-	# Initialize the MultiMesh
-	multimesh = MultiMesh.new()
-	multimesh.transform_format = MultiMesh.TRANSFORM_3D
-	multimesh.mesh = preload("res://straw.obj") # Assign your mesh
-	
-	# Set your desired limit (exceeding 65536)
-	var total_instances = 200000
-	multimesh.instance_count = total_instances
-	
-	# Populate the instances
-	for i in range(total_instances):
-		var random_x = randf_range(-100.0, 100.0)
-		var random_z = randf_range(-100.0, 100.0)
-		
-		var transform = Transform3D()
-		transform.origin = Vector3(random_x, 0, random_z)
-		
-		multimesh.set_instance_transform(i, transform)
+#func _ready():
+	## Initialize the MultiMesh
+	#multimesh = MultiMesh.new()
+	#multimesh.transform_format = MultiMesh.TRANSFORM_3D
+	#multimesh.mesh = preload("res://straw.obj") # Assign your mesh
+	#
+	## Set your desired limit (exceeding 65536)
+	#var total_instances = 200000
+	#multimesh.instance_count = total_instances
+	#
+	## Populate the instances
+	#for i in range(total_instances):
+		#var random_x = randf_range(-100.0, 100.0)
+		#var random_z = randf_range(-100.0, 100.0)
+		#
+		#var transform = Transform3D()
+		#transform.origin = Vector3(random_x, 0, random_z)
+		#
+		#multimesh.set_instance_transform(i, transform)
